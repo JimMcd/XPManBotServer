ae40fc9ba6f94a348f8ba9255a4cf78e6b8871a7
Mark Kirschstein <markkirschstein@hotmail.com>
2013-02-09 19:45:46 +0000

    added botfinder

---
+++ Server/Server.Tests/finding_bots_to_load.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+
+namespace Server.Tests
+{
+    [TestFixture]
+    public class finding_bots_to_load
+    {
+        [Test]
+        public void finds_bots_in_c_bots()
+        {
+            var foundBotsCount = new BotFinder().Find().Count;
+            Assert.That(foundBotsCount, Is.EqualTo(1));
+        }
+    }
+}
