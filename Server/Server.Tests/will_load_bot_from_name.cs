68cc6e641785b6fc426227fe84379739c3e5a0c8
Mark Kirschstein <markkirschstein@hotmail.com>
2013-02-09 19:57:00 +0000

    red test for getting string from ruby, make it pass jim

---
+++ Server/Server.Tests/will_load_bot_from_name.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Server.Tests
+{
+    [TestFixture]
+    public class will_load_bot_from_name
+    {
+        [Test]
+        public void loads_ruby_bot_and_gets_message_from_it()
+        {
+            var finder = new BotFinder();
+            var botname = finder.Find()[0];
+
+            var bot = new Bot(botname);
+
+            var result = bot.GetMessage();
+
+            Assert.That(result, Is.EqualTo("Hello Server"));
+        }
+    }
+
+    public class Bot
+    {
+        public Bot(string botname)
+        {
+            
+        }
+
+        public string GetMessage()
+        {
+            return "";
+        }
+    }
+}
