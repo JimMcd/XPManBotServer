9088c386e52badd39d3c5bc81491e13997d4f529
Jim <jimmeh.mcdonald+xp@gmail.com>
2013-02-10 19:18:25 +0000

    Added fixture creator

---
+++ Server/GameEngine/HeadsUp/ICreateBots.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameEngine.HeadsUp
+{
+    public interface ICreateBots
+    {
+        IAmABot Create(string botName);
+    }
+}
---
+++ Server/GameEngine/HeadsUpGameEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameEngine.HeadsUp;
+
+namespace GameEngine
+{
+    public class HeadsUpGameEngine
+    {
+        private readonly ICreateFixtures _botCreator;
+        private readonly ICreateHeadsUpGames _gameCreator;
+
+        public HeadsUpGameEngine(ICreateFixtures botCreator, ICreateHeadsUpGames gameCreator)
+        {
+            _botCreator = botCreator;
+            _gameCreator = gameCreator;
+        }
+    }
+}
