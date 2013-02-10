bc12001b4a517c80c18729a9cbc9ce177466700c
Jim <jimmeh.mcdonald+xp@gmail.com>
2013-02-10 19:08:20 +0000

    Seperated classes into files

--- Server/GameEngine/HeadsUp/ICreateHeadsUpGames.cs
+++ Server/GameEngine/HeadsUp/ICreateHeadsUpGames.cs
@@ -6,17 +6,6 @@ using System.Threading.Tasks;
 
 namespace GameEngine.HeadsUp
 {
-    public interface IAmAHeadsUpGame
-    {
-        void Play();
-    }
-
-    public interface IAmABot
-    {
-        void GetMessage(string message);
-        string GetMessage();
-    }
-
     public interface ICreateHeadsUpGames
     {
         IAmAHeadsUpGame Create(IAmABot playerOne, IAmABot playerTwo);
